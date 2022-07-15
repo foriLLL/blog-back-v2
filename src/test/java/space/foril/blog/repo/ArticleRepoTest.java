0d86b3a765a014f70cee297ac1454c17d8b6e07b
foril <1571825323@qq.com>
2022-07-15 21:28:51 +0800

    chore: comment added

--- src/test/java/space/foril/blog/repo/ArticleRepoTest.java
+++ src/test/java/space/foril/blog/repo/ArticleRepoTest.java
@@ -16,30 +16,30 @@ public class ArticleRepoTest {
     private ArticleRepo articleRepo;
 
     @Test
-    public void insertTest(){
+    public void insertTest() {
         Article article = new Article("title", 1, new Date(), null, "content", null, "desc", 1);
         System.out.println(articleRepo.insertArticle(article));
     }
 
     @Test
-    public void deleteTest(){
+    public void deleteTest() {
         System.out.println(articleRepo.deleteById(245));
     }
 
     @Test
-    public void updateTest(){
+    public void updateTest() {
         Article article = new Article(246, "changedTitle", 2, null, null, "new content", null, "new desc", 1);
         System.out.println(articleRepo.updateArticle(article));
     }
 
     @Test
-    public void selectAllTest(){
+    public void selectAllTest() {
         articleRepo.findAll().forEach(article ->
                 System.out.println(article.getTitle()));
     }
 
     @Test
-    public void selectByIdTest(){
+    public void selectByIdTest() {
         System.out.println(articleRepo.findById(240).getTime());
     }
 }
