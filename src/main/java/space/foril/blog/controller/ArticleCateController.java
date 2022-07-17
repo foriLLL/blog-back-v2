cc634c55b9d7dde47d314863c11a194556860e73
foril <1571825323@qq.com>
2022-07-17 20:38:02 +0800

    ArticleCateController added

---
+++ src/main/java/space/foril/blog/controller/ArticleCateController.java
@@ -0,0 +1,53 @@
+package space.foril.blog.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import space.foril.blog.entity.ArticleCate;
+import space.foril.blog.entity.ResBody;
+import space.foril.blog.service.ArticleCateService;
+
+import java.util.List;
+
+@Api(tags = "文章类别API")
+@RestController
+@RequestMapping("/api/articleCate")
+class ArticleCateApiController {
+    private final ArticleCateService articleCateService;
+
+
+    @Autowired
+    public ArticleCateApiController(ArticleCateService articleCateService) {
+        this.articleCateService = articleCateService;
+    }
+
+    @ApiOperation("获取所有文章类别")
+    @GetMapping("")
+    public ResBody<List<ArticleCate>> getAllCates(){
+        return new ResBody<>(true, articleCateService.getAllCates(), "");
+    }
+}
+
+@Api(tags = "管理文章类别API")
+@RestController("/api/mani/articleCate")
+class ArticleCateManiController{
+    private final ArticleCateService articleCateService;
+
+    @Autowired
+    public ArticleCateManiController(ArticleCateService articleCateService) {
+        this.articleCateService = articleCateService;
+    }
+    @PutMapping("")
+    public ResBody<Boolean> insertCate(@RequestBody ArticleCate articleCate){
+        Boolean ifSuccessful = articleCateService.insertCate(articleCate);
+        return new ResBody<>(ifSuccessful, ifSuccessful, "");
+    }
+    @PostMapping("")
+    public ResBody<Boolean> updateCate(@RequestBody ArticleCate articleCate){
+        Boolean ifSuccessful = articleCateService.updateCate(articleCate);
+        return new ResBody<>(ifSuccessful, ifSuccessful, "");
+    }
+
+
+}
